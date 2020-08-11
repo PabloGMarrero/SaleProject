7e362c634542cdf6cba286bd9ec4436cb81952a8
unknown <pablo.g.marrero@gmail.com>
2020-08-10 21:03:43 -0300

    added person entity

---
+++ SaleProject.Entities/Sales/Person.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SaleProject.Entities.Sales
+{
+    public class Person
+    {
+        public int idperson { get; set; }
+        [Required]
+        public string typeperson { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The name must be higher than 3 and less than 50 characters.")]
+        public string nameperson { get; set; }
+        public string documenttype { get; set; }
+        public string numerdocument { get; set; }
+        public string addressperson { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
