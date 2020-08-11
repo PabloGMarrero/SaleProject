6784f2df88f6f98ec25199b9d1ec0aa43b7daef8
unknown <pablo.g.marrero@gmail.com>
2020-08-10 21:08:57 -0300

    added update view model person

---
+++ SaleProject.Web/Models/Sales/Person/UpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaleProject.Web.Models.Sales.Person
+{
+    public class UpdateViewModel
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string typeperson { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The name must be higher than 3 and less than 50 characters.")]
+        public string name { get; set; }
+        public string documenttype { get; set; }
+        public string documentnumber { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
