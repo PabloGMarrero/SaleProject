ba0311f5532fd2279971bdee576c391bdd0c12c3
unknown <pablo.g.marrero@gmail.com>
2020-08-10 21:04:20 -0300

    added create view model and person view model

---
+++ SaleProject.Web/Models/Sales/Person/PersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleProject.Web.Models.Sales.Person
+{
+    public class PersonViewModel
+    {
+        public int idperson { get; set; }
+        public string typeperson { get; set; }
+        public string name { get; set; }
+        public string documenttype { get; set; }
+        public string documentnumber { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
