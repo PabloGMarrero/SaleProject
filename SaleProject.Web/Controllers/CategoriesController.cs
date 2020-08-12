60a8e59018871a565bbf7ab1fb80cb4446575320
unknown <pablo.g.marrero@gmail.com>
2020-08-12 15:40:56 -0300

    added authorization role for admin and grocerykeeper

--- SaleProject.Web/Controllers/CategoriesController.cs
+++ SaleProject.Web/Controllers/CategoriesController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SaleProject.Data;
@@ -11,6 +11,7 @@ using SaleProject.Web.Models.Stores.Category;
 
 namespace SaleProject.Web.Controllers
 {
+    [Authorize(Roles ="Admin, Grocerykeeper")]
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
