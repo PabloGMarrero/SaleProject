86f436f614350674bfaf5153bd677a48fcc6280e
unknown <pablo.g.marrero@gmail.com>
2020-08-12 16:57:16 -0300

    added income and detail income entities

---
+++ SaleProject.Entities/Store/DetailIncome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SaleProject.Entities.Store
+{
+    public class DetailIncome
+    {
+        public int iddetailincome { get; set; }
+        [Required]
+        public int idincome { get; set; }
+        [Required]
+        public int iditem { get; set; }
+        [Required]
+        public int amount { get; set; }
+        [Required]
+        public decimal price { get; set; }
+        public Income income { get; set; }
+    }
+}
---
+++ SaleProject.Entities/Store/Income.cs
@@ -0,0 +1,33 @@
+﻿using SaleProject.Entities.Sales;
+using SaleProject.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SaleProject.Entities.Store
+{
+    public class Income
+    {
+        public int idincome { get; set; }
+        [Required]
+        public int idsupplier { get; set; }
+        [Required]
+        public int iduser { get; set; }
+        [Required]
+        public string vouchertype  { get; set; }
+        public string voucherserie { get; set; }
+        [Required]
+        public string vouchernumber { get; set; }
+        [Required]
+        public DateTime dateincome { get; set; }
+        public decimal tax { get; set; }
+        [Required]
+        public decimal total { get; set; }
+        [Required]
+        public string stateincome { get; set; }
+        public ICollection<DetailIncome> detailIncomes { get; set; }
+        public User user { get; set; }
+        public Person person { get; set; }
+}
+}
