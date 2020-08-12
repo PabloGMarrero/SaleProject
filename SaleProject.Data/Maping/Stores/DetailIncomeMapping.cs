6f4a336001b42e3cb4b9d6f794be3f1bd0f23e75
unknown <pablo.g.marrero@gmail.com>
2020-08-12 20:12:58 -0300

    Added mapping to handle the detail income mapping to detail income entity

---
+++ SaleProject.Data/Maping/Stores/DetailIncomeMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaleProject.Entities.Store;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaleProject.Data.Maping.Stores
+{
+    public class DetailIncomeMapping : IEntityTypeConfiguration<DetailIncome>
+    {
+        public void Configure(EntityTypeBuilder<DetailIncome> builder)
+        {
+            builder.ToTable("detailincome").HasKey(d => d.iddetailincome);
+        }
+    }
+}
