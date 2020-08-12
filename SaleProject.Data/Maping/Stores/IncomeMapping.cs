88ca8d2b642cf71ae316edfee3330db18dfffdea
unknown <pablo.g.marrero@gmail.com>
2020-08-12 20:12:41 -0300

    Added mapping to handle the income mapping to income entity

---
+++ SaleProject.Data/Maping/Stores/IncomeMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaleProject.Entities.Store;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaleProject.Data.Maping.Stores
+{
+    public class IncomeMapping : IEntityTypeConfiguration<Income>
+    {
+        public void Configure(EntityTypeBuilder<Income> builder)
+        {
+            builder.ToTable("income").HasKey(i => i.idincome);
+            builder.HasOne(p => p.person)
+                .WithMany(i => i.incomes)
+                .HasForeignKey(i => i.idsupplier);
+        }
+    }
+}
