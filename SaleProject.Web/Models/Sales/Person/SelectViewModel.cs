afaabe1961e810a8fd4912de4c9da33641f476c9
unknown <pablo.g.marrero@gmail.com>
2020-08-27 01:26:35 -0300

    added select view model on person model

---
+++ SaleProject.Web/Models/Sales/Person/SelectViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleProject.Web.Models.Sales.Person
+{
+    public class SelectViewModel
+    {
+        public int idperson { get; set; }
+        public string namePerson { get; set; }
+    }
+}
